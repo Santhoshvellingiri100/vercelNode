3b9fb8fcc048122ebea445243b76281c4f43eb79
santhoshvellingiri100 <santhoshvellingiri100@gmail.com>
2024-10-20 10:40:25 +0530

    fix:app rate setter

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const express = require('express');
 // const serverless = require("serverless-http");
 const userRoute = require('./router/users')
 const fileRoute = require('./router/fileDownload')
-const gmail = require('./router/gmail')
+const gmail = require('./router/gmail');
+const { default: rateLimit } = require('express-rate-limit');
 const app = express();
 
+
+const limiter = rateLimit({
+     
+	windowMs: 1 * 60 * 1000, // 15 minutes
+     limit:5,
+      message: "Too many requests from this IP, please try again after a minute for next set",
+})
+app.use('/',limiter);
+
+
+
+//  routes
+
+// testiing
 app.get('/',(req,res)=>{
     res.jsonp({data:"hello santhosh"});
 })
 
+// routers
 app.use('/user',userRoute);
 app.use('/file',fileRoute);
 app.use('/gmail',gmail);
